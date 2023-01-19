6b1dc4508b80dad6f01cac7e0e4b418896ad2cab
LiuCodeDream <1193471389@qq.com>
2023-01-19 09:43:58 +0800

    前端初始化

---
+++ front/glasses_manager/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <ele-config-provider license="dk9mcwJyetRWQlxWRiojIzJCLi8mcQ5Wa3ojI0NWZqJWd6ICZpJCLzkjM3IXexAlcYdnIvl2cyVmdiwiIiATMuEjI6IibQf0NW==">
+    
+  </ele-config-provider>
+</template>
